168c9b72d21fb1e76676bd49d7bd9aa399a4bbcc
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-30 12:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
 
+    const form=document.getElementById("form1")
+	 let reqBody = {};
+	 Object.keys(form.elements).forEach(key => {
+        let element = form.elements[key];
+		 if (element.type !== "submit") {
+            reqBody[element.name] = element.value;
+        }
+	 })
+alert(`${reqBody['fname']} ${reqBody['lname']}`)
 }
